6fa8eeaaaa0f2c32d9c9b644904ad8da228a745e
p1tonidze <p1t0nidze422@gmail.com>
2025-02-21 21:00:05 +0200

    fix2 installa-app.sh

--- install-app.sh
+++ install-app.sh
@@ -3,15 +3,7 @@
 # Script to silently install and start the todo web app on the virtual machine.
 # Note that all commands bellow are without sudo - that's because extention mechanism
 # runs scripts under root user.
-cd ~
-git clone https://github.com/v-shutov/devops_todolist_terraform_task.git
 
-mkdir /app
-cp -r ~/devops_todolist_terraform_task/app/* /app
-rm -rf ~/devops_todolist_terraform_task
-
-cd /app
-chmod +x start.sh
 # install system updates and isntall python3-pip package using apt. '-yq' flags are
 # used to suppress any interactive prompts - we won't be able to confirm operation
 # when running the script as VM extention.
@@ -21,7 +13,7 @@ apt-get install python3-pip -yq
 # Create a directory for the app and download the files.
 mkdir /app
 # make sure to uncomment the line bellow and update the link with your GitHub username
-# git clone https://github.com/p1tinidze/adevops_todolist_terraform_task.git
+git clone https://github.com/p1tonidze/devops_todolist_terraform_task
 cp -r devops_todolist_terraform_task/app/* /app
 
 # create a service for the app via systemctl and start the app
